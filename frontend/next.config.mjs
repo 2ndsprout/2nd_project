56e4986328014296312e6bb392f027724ab36892
2ndsprout <2ndsprout@gmail.com>
2024-07-04 17:19:39 +0900

    frontend fix

---
+++ frontend/next.config.mjs
@@ -0,0 +1,38 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+	// reactStrictMode: true,
+	eslint: {
+		ignoreDuringBuilds: true,
+	},
+	async headers() {
+		return [
+			{
+				source: '/usports/:path*',
+				headers: [
+					{ key: 'Access-Control-Allow-Credentials', value: 'true' },
+					{ key: 'Access-Control-Allow-Origin', value: '*' },
+					{
+						key: 'Access-Control-Allow-Methods',
+						value: 'GET,OPTIONS,PATCH,DELETE,POST,PUT',
+					},
+					{
+						key: 'Access-Control-Allow-Headers',
+						value:
+							'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version',
+					},
+				],
+			},
+		]
+	},
+	async rewrites() {
+		return [
+			{
+				source: '/api/:path*',
+				destination: 'http://localhost:8080/api/:path*',
+			}
+		]
+	},
+	trailingSlash: true
+};
+
+export default nextConfig;
