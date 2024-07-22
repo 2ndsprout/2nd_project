9124ca29e367f238c50e03179c17c81fea189ec6
2ndsprout <2ndsprout@gmail.com>
2024-07-22 17:48:43 +0900

    mypage ing

--- frontend/src/app/Global/layout/ProfileLayout.tsx
+++ frontend/src/app/Global/layout/ProfileLayout.tsx
@@ -12,18 +12,14 @@ export default function Profile(props: pageInterface) {
     const profile = props.profile;
     return (
         <Main user={user} profile={profile} categories={props.categories}>
-        <div className="flex flex-col w-[1240px]">
-            <div className="flex w-full items-end">
-                <label className="font-bold text-2xl w-[185px] min-w-[185px]">나의 52번가</label>
-                <label className="text-xs">52번가 속 내 정보를 한번에 확인하세요!</label>
-            </div>
-            <div className="flex">
-                <SideList user={user} profile={profile} />
-                <div className="flex flex-col mt-4 w-full">
-                    {props.children}
+            <div className="flex flex-col w-[1500px]">
+                <div className="flex">
+                    <SideList user={user} profile={profile} />
+                    <div className="flex flex-col mt-4 w-full">
+                        {props.children}
+                    </div>
                 </div>
             </div>
-        </div>
-    </Main>
+        </Main>
     );
 }
--- frontend/src/app/account/mypage/page.tsx
+++ frontend/src/app/account/mypage/page.tsx
@@ -2,17 +2,24 @@
 
 import { useEffect, useState } from "react";
 import { redirect } from "next/navigation";
-import { getProfile, getUser } from "@/app/API/UserAPI";
+import { getProfile, getUser, saveImage, saveProfileImage } from "@/app/API/UserAPI";
 import Profile from "@/app/Global/layout/ProfileLayout";
 
-interface pageProps {
-  categories: any[];
-}
 
-export default function Page(props: pageProps) {
+export default function Page() {
   const [user, setUser] = useState(null as any);
   const [profile, setProfile] = useState(null as any);
-  const [categories, setCategories] = useState(props.categories);
+  const [categories, setCategories] = useState([] as any []);
+  const [url, setUrl] = useState('');
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [newPassword, setNewPassword] = useState('');
+  const [error, setError] = useState('');
+  const [username, setUsername] = useState('');
+  const [aptNum, setAptNum] = useState('');
+  const [roadAddress, setRoadAddress] = useState('');
+  const [aptName, setAptName] = useState('');
   const ACCESS_TOKEN = typeof window == 'undefined' ? null : localStorage.getItem('accessToken');
   const PROFILE_ID = typeof window == 'undefined' ? null : localStorage.getItem('PROFILE_ID');
 
@@ -21,12 +28,19 @@ export default function Page(props: pageProps) {
       getUser()
         .then(r => {
           setUser(r);
+          setEmail(r.email);
+          setUsername(r.username);
+          setAptNum(r.aptNum);
+          setRoadAddress(r.aptResponseDTO.roadAddress);
+          setAptName(r.aptResponseDTO.aptName);
         })
         .catch(e => console.log(e));
       if (PROFILE_ID)
         getProfile()
           .then(r => {
             setProfile(r);
+            setName(r.name);
+            setUrl(r.url);
             // getSearch({ Page: props.page, Keyword: encodeURIComponent(props.keyword)})
             // .then(r => setSearch(r))
             // .catch(e => console.log
@@ -40,11 +54,33 @@ export default function Page(props: pageProps) {
 
   }, [ACCESS_TOKEN, PROFILE_ID]);
 
+  function Change(file: any) {
+    const formData = new FormData();
+    formData.append('file', file);
+    saveImage(formData)
+        .then(r => setUrl(r?.url))
+        .catch(e => console.log(e));
+}
+
   return (
-    <Profile user={user} profile={profile} categories={props.categories}>
-      <div className='flex items-end'>
-        <label className='text-xl font-bold'><label className='text-xl text-red-500 font-bold'>회원정보</label> 변경</label>
-        <label className='text-xs h-[14px] border-l-2 border-gray-400 ml-2 mb-[5px] pl-2'>고객님의 회원정보를 수정하실 수 있습니다. 회원정보를 변경하시고 반드시 하단에 있는 <label className='font-bold'>확인</label> 버튼을 클릭해 주셔야 합니다.</label>
+    <Profile user={user} profile={profile} categories={categories}>
+      <div className='flex flex-col'>
+        <label className='text-xl font-bold'><label className='text-xl text-secondary font-bold'>회원정보</label> 변경</label>
+        <div className="mt-9 w-[1300px] border-2 h-[600px] overflow-y-scroll rounded-lg">
+          <div className="py-5 px-5">
+            <div className="relative w-full h-auto flex justify-center items-center mb-10">
+              <div className="w-[128px] h-[128px] rounded-full opacity-30 absolute hover:bg-gray-500" onClick={() => document.getElementById('file')?.click()}></div>
+              <img src={url == '' ? url : '/user.png'} alt='main Image' className='w-[128px] h-[128px] rounded-full' />
+              <input id='file' hidden type='file' onChange={e => Change(e.target.files?.[0])} />
+            </div>
+            <div>
+              fdsafdsa
+            </div>
+            <div>
+              fdsfafdsafdsfdsa
+            </div>
+          </div>
+        </div>
       </div>
     </Profile>
   );
