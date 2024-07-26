930c82e681b4bcdc3aa1354b836cdfe952fb4ef7
2ndsprout <2ndsprout@gmail.com>
2024-07-26 16:46:51 +0900

    culture_center id page fix

--- frontend/src/app/account/culture_center/[id]/page.tsx
+++ frontend/src/app/account/culture_center/[id]/page.tsx
@@ -1,12 +1,13 @@
 'use client';
 
 import { getProfile, getUser, getLessonList, getCenter } from "@/app/API/UserAPI";
-import Main from "@/app/Global/layout/MainLayout";
+import Main from "@/app/Global/layout/mainLayout";
 
-import { redirect, useParams } from "next/navigation";
+import { redirect, useParams, useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
 import Link from "next/link";
 import { getDateFormat } from "@/app/Global/component/Method";
+import Router from "next/router";
 
 interface Lesson {
     id: number;
@@ -29,6 +30,7 @@ interface PageProps {
 }
 
 export default function Page(props: PageProps) {
+    const router = useRouter();
     const params = useParams();
     const [user, setUser] = useState(null as any);
     const [profile, setProfile] = useState(null as any);
@@ -41,6 +43,7 @@ export default function Page(props: PageProps) {
     const [currentPage, setCurrentPage] = useState(1);
     const [totalPages, setTotalPages] = useState(1);
     const [totalElements, setTotalElements] = useState(0);
+    const [isLoading, setIsLoading] = useState(false);
 
     const countTotalLesson = (lessonList: any[]): number => {
         return lessonList.reduce((total, lesson) => {
@@ -54,7 +57,6 @@ export default function Page(props: PageProps) {
             getUser()
                 .then(r => {
                     setUser(r);
-                    console.log(r);
                 })
                 .catch(e => console.log(e));
             if (PROFILE_ID) {
@@ -66,11 +68,11 @@ export default function Page(props: PageProps) {
                 getCenter(centerId)
                     .then(r => {
                         setCenter(r);
-                        console.log('center', r);
                         return getLessonList(r.id, 0);
                     })
                     .then(r => {
                         setLessonList(r.content);
+                        const interval = setInterval(() => { setIsLoading(true); clearInterval(interval) }, 100);
                     })
                     .catch(e => console.log(e));
             } else {
@@ -83,7 +85,7 @@ export default function Page(props: PageProps) {
 
 
     return (
-        <Main user={user} profile={profile}>
+        <Main user={user} profile={profile} isLoading={isLoading}>
             <div className="bg-black w-full min-h-screen text-white flex h-full">
                 <aside className="w-1/6 p-6">
                     <div className="mt-5 ml-20 flex flex-col items-end">
@@ -93,36 +95,26 @@ export default function Page(props: PageProps) {
                         </div>
                     </div>
                 </aside>
-                <div className="w-full p-6 mt-[50px] flex h-full flex-col space-y-10 items-center">
-                    <td className="w-[1000px] flex items-center">
-                        <th className="ml-[130px] text-orange-400 w-[100px] flex justify-center border-b">수업 강사</th>
-                        <th className="ml-[130px] text-orange-400 w-[100px] flex justify-center border-b">수업 이름</th>
-                        <th className="ml-[300px] text-orange-400 w-[100px] flex justify-center border-b">수업 기간</th>
-                    </td>
+                <table className="w-full p-6 mt-[50px] flex h-full flex-col space-y-10 items-center">
+                    <thead>
+                        <tr className="w-[1000px] flex items-center">
+                            <th className="ml-[130px] text-orange-400 w-[100px] flex justify-center border-b">수업 강사</th>
+                            <th className="ml-[130px] text-orange-400 w-[100px] flex justify-center border-b">수업 이름</th>
+                            <th className="ml-[300px] text-orange-400 w-[100px] flex justify-center border-b">수업 기간</th>
+                        </tr>
+                    </thead>
                     {lessonList.map((lesson, index) => (
-                        <div key={index}>
-                            {/* <div className="bg-gray-800 p-4 rounded-lg w-[1000px] flex items-center h-[200px]">
-                                <div className="flex flex-col w-[150px] h-full justify-center">
-                                    <img src={lesson.profileResponseDTO?.url ? lesson.profileResponseDTO.url : '/user.png'} className="ml-[15px] w-[100px] flex h-2/3 justify-center rounded-full" alt="profile" />
-                                    <div className="w-[100px] h-1/3 flex items-center justify-center ml-[15px]">{lesson.profileResponseDTO.name}</div>
-                                </div>
-                                <div className="flex flex-col ml-[50px] h-full w-[700px]">
-                                    <h3 className="text-xl font-bold h-1/4 w-full text-orange-300 flex justify-center">{lesson.name}</h3>
-                                    <label className="flex h-3/4 w-full items-center justify-center">{getDateTimeFormat(lesson.startDate)} ~ {getDateTimeFormat(lesson.endDate)}</label>
-                                </div>
-                            </div> */}
-                            <Link href={`/account/lesson/${lesson.id}`} className="">
-                                <div className="bg-gray-800 p-2 rounded-lg w-[1000px] flex items-center h-[120px]">
-                                    <img src={lesson.profileResponseDTO?.url ? lesson.profileResponseDTO.url : '/user.png'} className="ml-[15px] w-[100px] flex h-full justify-center rounded-full" alt="profile" />
-                                    <div className="w-[200px] h-1/3 flex items-center justify-center ml-[15px]">{lesson.profileResponseDTO.name}</div>
-                                    <h3 className="text-xl font-bold h-1/4 w-full text-orange-300 flex justify-center">{lesson.name}</h3>
-                                    <label className="flex h-3/4 w-full items-center justify-center">{getDateFormat(lesson.startDate)} ~ {getDateFormat(lesson.endDate)}</label>
-                                </div>
-                            </Link>
-                        </div>
+                        <tbody key={index}>
+                            <tr className="bg-gray-800 p-2 rounded-lg w-[1000px] flex items-center h-[120px] hover:cursor-pointer"
+                            onClick={() => router.push(`/account/lesson/${lesson.id}`)}>
+                                <td><img src={lesson.profileResponseDTO?.url ? lesson.profileResponseDTO.url : '/user.png'} className="ml-[15px] w-[100px] flex h-full justify-center rounded-full" alt="profile" /></td>
+                                <td><div className="w-[200px] h-1/3 flex items-center justify-center ml-[15px]">{lesson.profileResponseDTO.name}</div></td>
+                                <td><h3 className="text-xl font-bold h-1/4 w-full text-orange-300 flex justify-center">{lesson.name}</h3></td>
+                                <td className="ml-48 flex h-3/4 w-full items-center justify-center">{getDateFormat(lesson.startDate)} ~ {getDateFormat(lesson.endDate)}</td>
+                            </tr>
+                        </tbody>
                     ))}
-                </div>
-
+                </table>
             </div>
         </Main>
     );
