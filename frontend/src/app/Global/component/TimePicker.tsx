cbcf1f80880b7e4ca3fe0c2c403c8b1c44dca131
dlfmalr <sonhyeseung05@gmail.com>
2024-07-30 17:45:06 +0900

    main page urlList fix

--- frontend/src/app/Global/component/TimePicker.tsx
+++ frontend/src/app/Global/component/TimePicker.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 import { DemoContainer } from '@mui/x-date-pickers/internals/demo';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
