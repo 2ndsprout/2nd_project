3adf2c77de211682604989f0cd6e5ec00b59557d
dlfmalr <161660992+dlfmalr@users.noreply.github.com>
2024-08-05 15:03:29 +0900

    Update AxiosAPI.tsx

--- frontend/src/app/API/AxiosAPI.tsx
+++ frontend/src/app/API/AxiosAPI.tsx
@@ -3,7 +3,7 @@ import axios from 'axios';
 export function getAPI() {
     const api = axios.create({
         // baseURL: 'http://localhost:8080',
-        baseURL: 'http://localhost:8080',
+        baseURL: 'http://15.164.75.8:8080',
         headers: {
             'Content-Type': 'application/json;charset=utf-8;',
         },
