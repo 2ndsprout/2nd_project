3d7f2133ea045fcedc4a65462f9e3b6eaba141df
hjh010806 <010806hjh@naver.com>
2024-08-05 16:16:45 +0900

    jun

--- frontend/src/app/account/admin/user/page.tsx
+++ frontend/src/app/account/admin/user/page.tsx
@@ -1,9 +1,12 @@
 'use client'
 
-import { getAptList, getCenterList, getUserList, deleteUser } from '@/app/API/UserAPI';
+import { getAptList, getCenterList, getUserList, deleteUser, register } from '@/app/API/UserAPI';
 
 import { getProfile, getUser } from "@/app/API/UserAPI";
+import ConfirmModal from '@/app/Global/component/ConfirmModal';
+import Modal from '@/app/Global/component/Modal';
 import Pagination from '@/app/Global/component/Pagination';
+import useConfirm from '@/app/Global/hook/useConfirm';
 import Admin from "@/app/Global/layout/AdminLayout";
 import { redirect } from "next/navigation";
 import { useEffect, useState } from "react";
@@ -20,11 +23,21 @@ export default function Page() {
     const [userList, setUserList] = useState([] as any[]);
     const [redirectCountdown, setRedirectCountdown] = useState<number | null>(null);
     const [error, setError] = useState('');
-    const [selectedApt, setSelectedApt] = useState(null as any);
+    const [selectedApt, setSelectedApt] = useState('' as any);
     const [aptError, setAptError] = useState('아파트를 설정해주세요.');
     const [apt, setApt] = useState<any>(null);
     const [currentPage, setCurrentPage] = useState(1);
     const [totalPages, setTotalPages] = useState(1);
+    const [isModalOpen, setISModalOpen] = useState(-1);
+    const [first, setFirst] = useState(true);
+    const [usernameError, setUsernameError] = useState('이름을 입력해주세요.');
+    const [aptNumberError, setAptNumError] = useState('아파트 동을 입력해주세요.');
+    const [roleError, setRoleError] = useState('권한을 입력해주세요.');
+    const [username, setUsername] = useState('' as string);
+    const [password, setPassword] = useState('' as string);
+    const [aptNum, setAptNum] = useState('' as any);
+    const [role, setRole] = useState('' as any);
+    const {confirmState, finalConfirm, closeConfirm } = useConfirm();
 
     useEffect(() => {
         if (ACCESS_TOKEN) {
@@ -121,6 +134,89 @@ export default function Page() {
             })
             .catch(e => console.log(e));
     }
+    function closeModal() {
+        setISModalOpen(-1);
+    }
+    function openModal(aptId: number) {
+        setISModalOpen(1)
+    }
+    function checkInput(
+        value: string,
+        pattern: RegExp,
+        onValid: () => void,
+        onInvalid: (error: string) => void,
+        error: string // error 메시지를 추가로 인수로 받습니다.
+    ) {
+        if (pattern.test(value)) {
+            onValid();
+        } else {
+            onInvalid(error); // error 메시지를 전달합니다.
+        }
+    }
+    const validateInput = (fieldName: string, value: string) => {
+        switch (fieldName) {
+            case 'username':
+                checkInput(
+                    value,
+                    /^[0-9a-zA-Z_]{2,20}$/,
+                    () => setUsernameError(''),
+                    (e) => setUsernameError(e),
+                    '이름은 2자에서 20자 이내로 숫자, 영어, 언더바(_)만 포함되게 작성해주세요.'
+                );
+                break;
+            case 'aptNumber':
+                checkInput(
+                    value,
+                    /^[0-9]{2,10}$/,
+                    () => setAptNumError(''),
+                    (e) => setAptNumError(e),
+                    '아파트 동은 10자 이내로 숫자만 포함되게 작성해주세요.'
+                );
+                break;
+            case 'aptId':
+                if (value === null || value === '') {
+                    setAptError('아파트 ID를 선택해주세요.');
+                } else {
+                    setAptError('');
+                }
+                break;
+            case 'role':
+                if (value === null || value === '') {
+                    setRoleError('역할을 선택해주세요.');
+                } else {
+                    setRoleError('');
+                }
+                break;
+            default:
+                break;
+        }
+    };
+
+    function submit() {
+        register({
+            name: username,
+            password: password,
+            aptNumber: aptNum,
+            aptId: selectedApt.aptId,
+            role: role,
+            email: '',
+            min: 0,
+            max: 0,
+            h: 0,
+            w: 0
+        })
+            // .then(() => window.location.href = '/propose')
+            .catch(e => console.log(e));
+    }
+
+    const handleChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+        setRole(Number(event.target.value) || ''); // 변환된 숫자값 설정
+    };
+
+    const allErrors = () => {
+        const errors = [usernameError, aptNumberError];
+        return errors.find(error => error !== '') || '';
+    };
 
     return (
         <Admin user={user} profile={profile} isLoading={isLoading}>
@@ -129,7 +225,7 @@ export default function Page() {
                     <div className='w-[1500px] h-[800px] bg-gray-700 rounded-lg items-center justify-center flex flex-col'>
                         <div className='flex w-[1000px]'>
                             <select
-                                className="flex mb-[30px] font-bold text-white select select-bordered w-full max-w-xs"
+                                className="flex mb-[30px] font-bold text-white select select-bordered w-[320px] max-w-xs"
                                 value={selectedApt}
                                 onChange={handleSelectChange}  // handleChange 함수를 호출합니다.
                             >
@@ -142,11 +238,73 @@ export default function Page() {
                                         key={apt.aptId}
                                         value={apt.aptId}
                                     >
-                                        {apt.aptId}
+                                        {apt.aptId} - {apt.aptName}
                                     </option>
                                 ))}
                             </select>
+                            <div className='w-[680px] flex justify-end'>
+                                <button className='flex items-center justify-center w-[100px] bg-yellow-600 rounded hover:bg-yellow-400 h-[45px]' onClick={() => openModal(selectedApt.aptId)}>
+                                    유저 생성
+                                </button>
+                            </div>
                         </div>
+                        <Modal open={isModalOpen === 1} onClose={() => setISModalOpen(-1)} className='w-[900px] h-[700px] flex flex-col justify-center items-center p-5' escClose={true} outlineClose={true} >
+                            <button className="btn btn-xl btn-circle text-xl text-black btn-ghost absolute right-2 top-2 hover:cursor-pointer" onClick={() => closeModal()}> ✕ </button>
+                            <div className="w-full flex items-center mb-[30px]">
+                                <span className="text-xl font-bold text-black w-[150px]">Apt Id<span className="ml-[62px]">:</span></span>
+                                <select
+                                    className="flex font-bold text-black select select-bordered w-[320px] max-w-xs"
+                                    value={selectedApt}
+                                    onChange={handleSelectChange}  // handleChange 함수를 호출합니다.
+                                >
+                                    <option className="text-black font-bold" value="">
+                                        아파트 목록
+                                    </option>
+                                    {aptList.map((apt) => (
+                                        <option
+                                            className="text-black"
+                                            key={apt.aptId}
+                                            value={apt.aptId}
+                                        >
+                                            {apt.aptId} - {apt.aptName}
+                                        </option>
+                                    ))}
+                                </select>
+                            </div>
+                            <div className="w-full flex items-center mb-[30px]">
+                                <span className="text-xl font-bold text-black w-[150px]">name<span className="ml-[62px]">:</span></span>
+                                <input type="text"
+                                    onChange={e => { if (first) setFirst(false); setUsername(e.target.value); setPassword(e.target.value); validateInput('username', (e.target as HTMLInputElement).value); }}
+                                    onFocus={(e) => { validateInput('username', (e.target as HTMLInputElement).value); if (e.target.value === '') setUsernameError('이름을 입력해주세요.') }}
+                                    onKeyUp={(e) => { validateInput('username', (e.target as HTMLInputElement).value); if ((e.target as HTMLInputElement).value === '') setUsernameError('이름을 입력해주세요.') }}
+                                    placeholder="아파트아이디_동호수 ex) 1_101101" className="text-black text-lg w-[650px] h-[50px] bg-white border border-gray-400 rounded-lg p-2.5" />
+                            </div>
+                            <div className="w-full flex items-center mb-[30px]">
+                                <span className="text-lg font-bold text-black w-[150px]">Apt Num<span className="ml-[40px]">:</span></span>
+                                <input type="text"
+                                    onChange={e => { if (first) setFirst(false); setAptNum(e.target.value); validateInput('aptNumber', (e.target as HTMLInputElement).value); }}
+                                    onFocus={(e) => { validateInput('aptNumber', (e.target as HTMLInputElement).value); if (e.target.value === '') setAptNumError('아파트 동을 입력해주세요.') }}
+                                    onKeyUp={(e) => { validateInput('aptNumber', (e.target as HTMLInputElement).value); if ((e.target as HTMLInputElement).value === '') setUsernameError('아파트 동을 입력해주세요.') }}
+                                    placeholder="ex) 101" className="text-black text-lg w-[650px] h-[50px] bg-white border border-gray-400 rounded-lg p-2.5" />
+                            </div>
+                            <div className="w-full flex items-center">
+                                <span className="text-lg font-bold text-black w-[150px] ">Role<span className="ml-[78px]">:</span></span>
+                                <select
+                                    id="role"
+                                    value={role}
+                                    onChange={handleChange}
+                                    className="text-black text-lg w-[650px] h-[50px] bg-white border border-gray-400 rounded-lg p-2.5"
+                                >
+                                    <option value="">역할을 선택하세요</option>
+                                    <option value={1}>관리자</option>
+                                    <option value={2}>강사</option>
+                                    <option value={3}>주민</option>
+                                </select>
+                            </div>
+                            <button className="ml-[200px] w-[150px] btn btn-xl btn-accent mt-6 text-black" disabled={first || allErrors().length != 0} onClick={() => finalConfirm(aptNum, '회원을 만들겠습니까?', '신청완료', submit)}>
+                                신청
+                            </button>
+                        </Modal>
                         <div className="h-[500px] w-[1000px]">
                             {userList.map((user) => (
                                 <div key={user.username} className='flex items-center justify-between border-b-2 h-[50px]'>
@@ -156,7 +314,7 @@ export default function Page() {
                                         </div>
                                     </div>
                                     <div className="w-[300px] justify-end flex">
-                                        <button className='text-sm mr-[30px] font-bold text-orange-300 hover:text-orange-500' onClick={() => deleteUserButton(user.username)}>유저 삭제</button>
+                                        <button className='text-sm mr-[30px] font-bold text-red-400 hover:text-red-600' onClick={() => deleteUserButton(user.username)}>유저 삭제</button>
                                     </div>
                                 </div>
                             ))}
@@ -171,6 +329,7 @@ export default function Page() {
                     </div>
                 </div>
             </div>
+            <ConfirmModal title={confirmState?.title} content={confirmState?.content} confirm={confirmState?.confirm} show={confirmState?.show} onConfirm={confirmState?.onConfirm} onClose={closeConfirm} />
         </Admin>
     );
 }
